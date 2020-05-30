b57e55ec6712ffee439cb09611254f53035f2a80
Victor Popkov <victor@popkov.me>
2020-05-30 20:05:20 +0300

    Change content of the "Home" page

--- src/app/home/Home.jsx
+++ src/app/home/Home.jsx
@@ -1,14 +1,47 @@
 import { Col, Container, Row } from 'reactstrap';
 import React, { Component } from 'react';
 import Prism from 'prismjs';
+import ReactMarkdown from 'react-markdown';
 import { hot } from 'react-hot-loader';
 import { Jumbotron, Sidebar } from '../common';
 
 @hot(module)
 class Home extends Component {
+  content =
+    'This library has been originally intended to be a fork of the [react-icheck](https://github.com/luqin/react-icheck)\n' +
+    "created by [luqin](https://github.com/luqin) which didn't seem to be maintained anymore. However, after\n" +
+    'diving into the original code it became more convenient to create a new one as a\n' +
+    'drop-in replacement with some additional features.\n' +
+    '\n' +
+    '### Differences\n' +
+    '\n' +
+    'Below you can find the list of the main differences from the original\n' +
+    '[react-icheck](https://github.com/luqin/react-icheck) library:\n' +
+    '\n' +
+    '- Refactored and improved the existing codebase:\n' +
+    '\n' +
+    '  - Added `onBlur` and `onFocus` label events to accompany `onMouseOut` and `onMouseOver`\n' +
+    '  - Moved all corresponding properties from the `EnchantedSwitch` to both `Checkbox` and `Radio` components and removed unused ones\n' +
+    '  - Removed `EnchantedSwitch` in favour of `Input` and `Label` components\n' +
+    '  - Removed `refs` usage to follow the "[Don’t Overuse Refs](https://reactjs.org/docs/refs-and-the-dom.html#dont-overuse-refs)" guide\n' +
+    '\n' +
+    '- Introduced `CheckboxGroup` similar to the `RadioGroup`\n' +
+    '\n' +
+    '- Introduced `checkboxWrapClassName` and `checkboxWrapTag` properties for `RadioGroup` to control the wrapping behaviour of the radio elements\n' +
+    '\n' +
+    '- Introduced `labelTag` and `labelTagClassName` properties for both `Checkbox` and `Radio` components to control the rendering of the label content\n' +
+    '\n' +
+    '- Labels for `Checkbox` and `Radio` can be passed as children alongside with the original label property';
+
   componentDidMount() {
     Prism.highlightAll();
   }
+
+  renderShieldsBadge = (href, src, alt) => (
+    <a href={href} rel="noopener noreferrer" target="_blank">
+      <img alt={alt} src={`https://img.shields.io/${src}?style=flat-square`} />
+    </a>
+  );
 
   render() {
     return (
@@ -19,208 +52,38 @@ class Home extends Component {
             <Col md={9}>
               <h2 className="heading">Overview</h2>
               <p className="badges clearfix">
-                <a
-                  href="https://www.npmjs.com/package/react-ui-icheck"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <img
-                    alt="npm"
-                    src="https://img.shields.io/npm/v/react-ui-icheck.svg"
-                  />
-                </a>
-                <a
-                  href="https://david-dm.org/victorpopkov/react-ui-icheck"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <img
-                    alt="David"
-                    src="https://img.shields.io/david/victorpopkov/react-ui-icheck.svg"
-                  />
-                </a>
-                <a
-                  href="https://david-dm.org/victorpopkov/react-ui-icheck?type=dev"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <img
-                    alt="David"
-                    src="https://img.shields.io/david/dev/victorpopkov/react-ui-icheck.svg"
-                  />
-                </a>
-                <a
-                  href="https://travis-ci.org/victorpopkov/react-ui-icheck"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <img
-                    alt="Travis (.org)"
-                    src="https://img.shields.io/travis/victorpopkov/react-ui-icheck.svg"
-                  />
-                </a>
-                <a
-                  href="https://codecov.io/gh/victorpopkov/react-ui-icheck"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <img
-                    alt="Coveralls"
-                    src="https://img.shields.io/codecov/c/github/victorpopkov/react-ui-icheck.svg"
-                  />
-                </a>
-                <a
-                  href="https://codeclimate.com/github/victorpopkov/react-ui-icheck"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <img
-                    alt="Code Climate"
-                    src="https://img.shields.io/codeclimate/maintainability/victorpopkov/react-ui-icheck.svg"
-                  />
-                </a>
+                {this.renderShieldsBadge(
+                  'https://www.npmjs.com/package/react-ui-icheck',
+                  'npm/v/react-ui-icheck',
+                  'npm',
+                )}
+                {this.renderShieldsBadge(
+                  'https://david-dm.org/victorpopkov/react-ui-icheck',
+                  'david/victorpopkov/react-ui-icheck',
+                  'David',
+                )}
+                {this.renderShieldsBadge(
+                  'https://david-dm.org/victorpopkov/react-ui-icheck?type=dev',
+                  'david/dev/victorpopkov/react-ui-icheck',
+                  'David Dev',
+                )}
+                {this.renderShieldsBadge(
+                  'https://github.com/victorpopkov/react-ui-icheck/actions',
+                  'github/workflow/status/victorpopkov/react-ui-icheck/CI',
+                  'GitHub Workflow Status',
+                )}
+                {this.renderShieldsBadge(
+                  'https://codecov.io/gh/victorpopkov/react-ui-icheck',
+                  'codecov/c/github/victorpopkov/react-ui-icheck',
+                  'Codecov',
+                )}
+                {this.renderShieldsBadge(
+                  'https://codeclimate.com/github/victorpopkov/react-ui-icheck',
+                  'codeclimate/maintainability/victorpopkov/react-ui-icheck',
+                  'Code Climate',
+                )}
               </p>
-              <p>
-                {`This library was originally intended to be a fork of the `}
-                <a
-                  href="https://github.com/luqin/react-icheck"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  react-icheck
-                </a>
-                {` library created by `}
-                <a
-                  href="https://github.com/luqin"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  luqin
-                </a>
-                {` which doesn't seem to be actively maintained any more `}
-                <i>
-                  {`[at the moment of writing this, the last `}
-                  <a
-                    href="https://github.com/luqin/react-icheck"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                  >
-                    react-icheck
-                  </a>
-                  {` release (`}
-                  <a
-                    href="https://github.com/luqin/react-icheck/releases/tag/v0.3.6"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                  >
-                    v0.3.6
-                  </a>
-                  ) was 2 years ago]
-                </i>
-                .
-              </p>
-              <p>
-                After diving into the original code it was more convenient to
-                create a new separate one as a drop-in replacement with some
-                additional features.
-              </p>
-              <p>
-                {`Below you can find the list of the main differences from the
-                original `}
-                <a
-                  href="https://github.com/luqin/react-icheck"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  react-icheck
-                </a>
-                {` library.`}
-              </p>
-              <h3 className="mb-4">Differences</h3>
-              <ul>
-                <li>
-                  {`Completely refactored and fixed/improved the existing code: `}
-                  <ul className="mb-2 mt-2">
-                    <li>
-                      {`added `}
-                      <pre className="d-inline">onBlur</pre>
-                      {` and `}
-                      <pre className="d-inline">onFocus</pre>
-                      {` label events to accompany `}
-                      <pre className="d-inline">onMouseOut</pre>
-                      {` and `}
-                      <pre className="d-inline">onMouseOver</pre>
-                      {` for accessibility`}
-                    </li>
-                    <li>
-                      {`moved all corresponding properties from the `}
-                      <pre className="d-inline">EnchantedSwitch</pre>
-                      {` to both `}
-                      <pre className="d-inline">Checkbox</pre>
-                      {` and `}
-                      <pre className="d-inline">Radio</pre>
-                      {` components and removed unused ones`}
-                    </li>
-                    <li>
-                      {`removed `}
-                      <pre className="d-inline">EnchantedSwitch</pre>
-                      {` in favour of `}
-                      <pre className="d-inline">Input</pre>
-                      {` and `}
-                      <pre className="d-inline">Label</pre>
-                      {` components`}
-                    </li>
-                    <li>
-                      {`removed `}
-                      <pre className="d-inline">refs</pre>
-                      {` usage to follow the "`}
-                      <a
-                        href="https://reactjs.org/docs/refs-and-the-dom.html#dont-overuse-refs"
-                        rel="noopener noreferrer"
-                        target="_blank"
-                      >
-                        Don’t Overuse Refs
-                      </a>
-                      {`" guide`}
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  {`Introduced `}
-                  <pre className="d-inline">CheckboxGroup</pre>
-                  {` similar to the `}
-                  <pre className="d-inline">RadioGroup</pre>
-                </li>
-                <li>
-                  {`Introduced `}
-                  <pre className="d-inline">checkboxWrapClassName</pre>
-                  {` and `}
-                  <pre className="d-inline">checkboxWrapTag</pre>
-                  {` properties for `}
-                  <pre className="d-inline">RadioGroup</pre>
-                  {` to control the wrapping behaviour of the radio elements.`}
-                </li>
-                <li>
-                  {`Introduced `}
-                  <pre className="d-inline">labelTag</pre>
-                  {` and `}
-                  <pre className="d-inline">labelTagClassName</pre>
-                  {` properties for both `}
-                  <pre className="d-inline">Checkbox</pre>
-                  {` and `}
-                  <pre className="d-inline">Radio</pre>
-                  {` components to control the rendering of the label content.`}
-                </li>
-                <li>
-                  {`Labels for `}
-                  <pre className="d-inline">Checkbox</pre>
-                  {` and `}
-                  <pre className="d-inline">Radio</pre>
-                  {` can be passed as children alongside with the original `}
-                  <pre className="d-inline">label</pre>
-                  {` property.`}
-                </li>
-              </ul>
+              <ReactMarkdown source={this.content} />
             </Col>
             <Sidebar />
           </Row>

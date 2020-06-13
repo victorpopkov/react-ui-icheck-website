76f7738defcedf946ac754c1afc28223726973fe
Victor Popkov <victor@popkov.me>
2020-06-13 08:18:50 +0300

    Move jumbotron version into a separate JumbotronVersion component

--- src/app/common/jumbotron/Jumbotron.jsx
+++ src/app/common/jumbotron/Jumbotron.jsx
@@ -1,53 +1,28 @@
 import './Jumbotron.scss';
-import {
-  Badge,
-  Jumbotron as BaseJumbotron,
-  Col,
-  Container,
-  Row,
-} from 'reactstrap';
+import { Jumbotron as BaseJumbotron, Col, Container, Row } from 'reactstrap';
 import React from 'react';
 import { hot } from 'react-hot-loader';
 import config from '@WebsiteConfig';
+import JumbotronVersion from './JumbotronVersion';
 
 const Jumbotron = () => (
   <BaseJumbotron className="m-0 pb-5 pt-5" styleName="jumbotron" tag="section">
     <Container>
       <h1>{config.lib.name}</h1>
-      <h6 className="mt-4">
-        <span className="align-middle">Website version:</span>
-        {` `}
-        <Badge className="align-middle">{`${config.app.version}`}</Badge>
-        {config.app.revision && ` `}
-        {config.app.revision && (
-          <Badge color="light" className="align-middle">
-            <a
-              href={`https://github.com/victorpopkov/${config.app.name}/commit/${config.app.revision}`}
-              rel="noreferrer"
-              target="_blank"
-            >
-              {`${config.app.revision.substring(0, 7)}`}
-            </a>
-          </Badge>
-        )}
-      </h6>
-      <h6 className="mb-4">
-        <span className="align-middle">Library version:</span>
-        {` `}
-        <Badge className="align-middle">{config.lib.version}</Badge>
-        {config.lib.revision && ` `}
-        {config.lib.revision && (
-          <Badge color="light" className="align-middle">
-            <a
-              href={`https://github.com/victorpopkov/${config.lib.name}/commit/${config.lib.revision}`}
-              rel="noreferrer"
-              target="_blank"
-            >
-              {`${config.lib.revision.substring(0, 7)}`}
-            </a>
-          </Badge>
-        )}
-      </h6>
+      <JumbotronVersion
+        className="mt-4"
+        name={config.lib.name}
+        revision={config.lib.revision}
+        title="Library"
+        version={config.lib.version}
+      />
+      <JumbotronVersion
+        className="mb-4"
+        name={config.app.name}
+        revision={config.app.revision}
+        title="Website"
+        version={config.app.version}
+      />
       <Row className="justify-content-center">
         <Col lg={6} md={8} xl={4} xs={12}>
           <pre className="text-center">

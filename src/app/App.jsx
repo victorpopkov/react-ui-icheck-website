bd53b81b2df8eaf8d1e60e3b42970c3d310bf178
Victor Popkov <victor@popkov.me>
2020-06-22 22:15:23 +0300

    Migrate App component to become functional

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,25 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'icheck/skins/all.css';
 import './style.scss';
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { hot } from 'react-hot-loader';
+import { hot } from 'react-hot-loader/root';
 import { Footer, Navbar } from './common';
 
-@hot(module)
-class App extends Component {
-  render() {
-    const { children } = this.props;
-
-    return (
-      <div className="app">
-        <Navbar />
-        {children}
-        <Footer />
-      </div>
-    );
-  }
-}
+const App = ({ children }) => (
+  <div className="app">
+    <Navbar />
+    {children}
+    <Footer />
+  </div>
+);
 
 App.propTypes = {
   children: PropTypes.node,
@@ -29,4 +22,4 @@ App.defaultProps = {
   children: null,
 };
 
-export default App;
+export default hot(App);

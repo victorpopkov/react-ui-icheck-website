0d629e46aae2ed77aa6eb3359a732c125da7f2a0
Victor Popkov <victor@popkov.me>
2021-04-04 14:52:55 +0300

    Change GitHub buttons in the common Footer component

--- src/app/common/footer/Footer.jsx
+++ src/app/common/footer/Footer.jsx
@@ -3,35 +3,19 @@ import { Col, Container, Row } from 'reactstrap';
 import React from 'react';
 import { hot } from 'react-hot-loader';
 import config from '@WebsiteConfig';
+import GitHub from '../github/GitHub';
 
 const Footer = () => (
   <footer className="footer" styleName="footer">
     <Container>
       <Row>
         <Col md={12} styleName="content">
-          <ul>
+          <ul className="mb-2 mb-md-0 mr-0 mr-md-4">
             <li>
               <a href={config.lib.repository}>View on GitHub</a>
             </li>
           </ul>
-          <div styleName="buttons">
-            <iframe
-              frameBorder="0"
-              height="20px"
-              scrolling="0"
-              src={`https://ghbtns.com/github-btn.html?user=victorpopkov&repo=${config.lib.name}&type=star&count=true`}
-              title="GitHub Stars"
-              width="80px"
-            />
-            <iframe
-              frameBorder="0"
-              height="20px"
-              scrolling="0"
-              src={`https://ghbtns.com/github-btn.html?user=victorpopkov&repo=${config.lib.name}&type=watch&count=true&v=2`}
-              title="GitHub Watchers"
-              width="90px"
-            />
-          </div>
+          <GitHub />
         </Col>
       </Row>
     </Container>

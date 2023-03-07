38895b71bab33e9355c1ef5fcd6c0d1b020cf113
Victor Popkov <victor@popkov.me>
2023-03-07 10:11:45 +0200

    Replace .eslintrc with .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: ['airbnb', 'plugin:prettier/recommended'],
+  env: {
+    browser: true,
+    es6: true,
+  },
+  parser: '@babel/eslint-parser',
+  rules: {
+    'import/no-unresolved': [2, { ignore: ['@Config'] }],
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+  },
+};

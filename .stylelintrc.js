18709d1af9606f959b8d86b7c894eea6cb24c83f
Victor Popkov <victor@popkov.me>
2024-01-28 23:09:04 +0200

    Migrate from stylelint-config-prettier to stylelint-config-twbs-bootstrap
    - Add stylelint-config-twbs-bootstrap
    - Change stylelint configurations
    - Fix code linting issues
    - Remove deprecated stylelint-config-prettier
    - Remove stylelint-order
    - Remove stylelint-scss

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,14 @@
 module.exports = {
-  extends: ['stylelint-config-prettier'],
+  extends: ['stylelint-config-twbs-bootstrap'],
   customSyntax: 'postcss-scss',
-  plugins: ['stylelint-order', 'stylelint-scss'],
   ignoreFiles: ['dist/**/*.css'],
+  rules: {
+    'selector-max-id': null,
+    'selector-pseudo-class-no-unknown': [
+      true,
+      { ignorePseudoClasses: ['global'] },
+    ],
+    'stylistic/number-leading-zero': null,
+    'stylistic/string-quotes': 'single',
+  },
 };
